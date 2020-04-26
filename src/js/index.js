ae66ea3689d96c7dd13c21e6bdc7b3fa44567574
CristhianChaucaMariaca <cristhiancchm+git@gmail.com>
2020-04-25 22:14:03 -0400

    se añadio la carpeta media con 780px y se configuro en navegador para moviles

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import "../sass/index.scss";
 // import jpg from '../assets/'; FIXME: RESOLVER PROBLEMA DE IMPORTACIÓN DE IMÁGENES CON WEBPACK
+
+window.addEventListener('load', inicio);
+
+function inicio(){
+    document.getElementById('btn-menu').addEventListener('click', mostrarMenu);
+}
+function mostrarMenu(){
+    document.getElementById('nav-primary').classList.toggle('mostrar-nav')
+}
