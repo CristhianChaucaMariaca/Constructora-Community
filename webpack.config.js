f7262fef366b77226dd233874d42586a066e56d0
Cristhian Chauca <cristhiancchm+git@gmail.com>
2021-03-17 21:00:22 -0400

    Se crearon los templates y se generaron la paginas para la pagina web

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,56 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/index.js',
-    output:{
-        path: __dirname + '/dist',
-        filename: 'app.js'
-    },
-    devServer:{
-        port:5000,
-        contentBase:path.join(__dirname, 'dist')
-    },
-    module: {
-        rules:[
-            {
-                test:/.pug$/,
-                use:['pug-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader'},
-                    {loader: 'sass-loader'}
-                ]
-            },
-            // FIXME: Resolver problema de importación de archivos con webpack
-            // {
-            //     test: /\.(png|jpe?g|gif)$/i,
-            //     loader: 'file-loader',
-            //     options: {
-            //       outputPath: './dist/media/',
-            //       publicPath: './src/assets/',
-            //     },
-            // },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve('./src/pug/pages/','index.pug')
-        })
-    ]
-}
+  entry: "./src/js/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "app.js",
+  },
+  devServer: {
+    port: 5000,
+    contentBase: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+      // FIXME: Resolver problema de importación de archivos con webpack
+      // {
+      //     test: /\.(png|jpe?g|gif)$/i,
+      //     loader: 'file-loader',
+      //     options: {
+      //       outputPath: './dist/media/',
+      //       publicPath: './src/assets/',
+      //     },
+      // },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./src/pug/pages/", "index.pug"),
+    }),
+    new HtmlWebpackPlugin({
+      filename: "Servicios.html",
+      template: path.resolve("./src/pug/pages/", "servicios.pug"),
+    }),
+    new HtmlWebpackPlugin({
+      filename: "Nosotros.html",
+      template: path.resolve("./src/pug/pages/", "nosotros.pug"),
+    }),
+    new HtmlWebpackPlugin({
+      filename: "Contacto.html",
+      template: path.resolve("./src/pug/pages/", "contactos.pug"),
+    }),
+  ],
+};
